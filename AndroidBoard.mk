40e25f87816f2f70ca1592025504d328f06e14b1
franjhv <franjhv@yahoo.es>
2017-06-19 16:25:28 +0200

    Update AndroidBoard.mk
    adapt to gretel gt6000

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/doogee/x5max_pro/AndroidBoardVendor.mk
+-include vendor/doogee/hct6737m_65_n/AndroidBoardVendor.mk
